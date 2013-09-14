1f9d0574ab47452714bec7e997853fcb8c87bcd8
PJW <pengjingwen1994@163.com>
2013-09-15 00:26:07 +0800

    Update install-font-centos.sh

--- install-font-centos.sh
+++ install-font-centos.sh
@@ -9,7 +9,7 @@ echo "Entering Font Directory"
 cd /usr/share/fonts/monaco
 
 echo "Downloading font"
-wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
+wget -c https://github.com/pengjingwen1994/monaco-font/raw/master/Monaco_Linux.ttf
 
 echo "Installing font"
 chmod 644 Monaco_Linux.ttf
