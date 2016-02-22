ba74a14d8e914e72eaadff901b587a12a6a2c6db
Christian Strappazzon <c.strap@gmail.com>
2016-02-22 22:30:40 +0100

    Fake

--- install-font-ubuntu.sh
+++ install-font-ubuntu.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 echo "Start install"
-sudo mkdir -p /usr/share/fonts/truetype/custom
+#sudo mkdir -p /usr/share/fonts/truetype/custom
+mkdir -p /tmp/font/
 
 echo "Downloading font"
 url=$1
 wget -c $url
 
 echo "Installing font"
-sudo mv ${url##*/} /usr/share/fonts/truetype/custom/
+#sudo mv ${url##*/} /usr/share/fonts/truetype/custom/
+mv ${url##*/} /tmp/font/
 
 echo "Updating font cache"
-sudo fc-cache -f -v
+#sudo fc-cache -f -v
 
 echo "Enjoy"
