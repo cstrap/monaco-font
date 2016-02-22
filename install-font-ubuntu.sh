54dc40952007cfbf4e6dbb65759edb1331668445
Christian Strappazzon <c.strap@gmail.com>
2016-02-22 22:27:20 +0100

    Update install-font-ubuntu.sh

--- install-font-ubuntu.sh
+++ install-font-ubuntu.sh
@@ -4,10 +4,11 @@ echo "Start install"
 sudo mkdir -p /usr/share/fonts/truetype/custom
 
 echo "Downloading font"
-wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
+url=$1
+wget -c $url
 
 echo "Installing font"
-sudo mv Monaco_Linux.ttf /usr/share/fonts/truetype/custom/
+sudo mv ${url##*/} /usr/share/fonts/truetype/custom/
 
 echo "Updating font cache"
 sudo fc-cache -f -v
