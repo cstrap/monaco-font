d415bbf7a055ca1b7eb3800db06587d60f86f98d
Bruno <pazosbruno@gmail.com>
2023-11-19 11:28:04 -0300

    solved missing merge conflict

--- install-font-gentoo.sh
+++ install-font-gentoo.sh
@@ -8,17 +8,10 @@ echo "Start install"
 sudo mkdir -p $FONTDIR
 
 echo "Downloading font"
-<<<<<<< HEAD
-wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
-
-echo "Installing font"
-sudo mv Monaco_Linux.ttf $FONTDIR
-=======
 wget -c $URL
 
 echo "Installing font"
 sudo mv $FILENAME $FONTDIR
->>>>>>> 0518a323c3bf7b037a5c2e00e5ac26da2e7efdd3
 
 echo "Updating font cache"
 sudo fc-cache -f -v
